cd19319c08b402ba35f8077f5f14c46469d4ab19
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-20 14:06:30 -0800

    set up

--- noise.js
+++ noise.js
@@ -1,3 +1,8 @@
-$(document).ready( function() {
-  // your code here
+const audio = ["media/c_note.mp3", "media/d_note.mp3", "media/e_note.mp3", "media/f_note.mp3", "media/g_note.mp3", "media/a_note.mp3", "media/b_note.mp3"];
+
+$(document).ready(function() {
+  $('button').click(function() {
+      var i = $(this).attr('class').substring(5);
+      new Audio(audio[i-1]).play();
+  });
 });
